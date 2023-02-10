2d6204f9ab2a70afa8c62f7f54cfff1aaf2f633a
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2023-02-10 11:50:36 +0100

    Removed unused migrations.

--- remote_db/migrations_old/20221015143947_search_indexing.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX cell_name_trgm_idx ON cell USING GIN (name gin_trgm_ops);
-CREATE INDEX cell_description_trgm_idx ON cell USING GIN (description gin_trgm_ops);
-
-CREATE INDEX culture_name_trgm_idx ON culture USING GIN (name gin_trgm_ops);
-CREATE INDEX culture_description_trgm_idx ON culture USING GIN (description gin_trgm_ops);
-
-CREATE INDEX buyer_name_trgm_idx ON buyer USING GIN (name gin_trgm_ops);
-CREATE INDEX buyer_address_trgm_idx ON buyer USING GIN (address gin_trgm_ops);
-CREATE INDEX buyer_contact_trgm_idx ON buyer USING GIN (contact gin_trgm_ops);
